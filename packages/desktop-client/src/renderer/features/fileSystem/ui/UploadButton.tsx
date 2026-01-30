bfc4a5936fe9698f0dbb7fc822b202416ca539d4
QQ5YU <cicoeee91@gmail.com>
2026-01-30 08:33:50 +0000

    feat(file-system/upload-button): pass currentFolderId on upload
    Read `currentFolderId` from `useFileBrowserStore` and pass it to `uploadFile` so uploaded files are assigned to the selected folder. Await upload before clearing input.
    
    Signed-off-by: QQ5YU <cicoeee91@gmail.com>

--- packages/desktop-client/src/renderer/features/fileSystem/ui/UploadButton.tsx
+++ packages/desktop-client/src/renderer/features/fileSystem/ui/UploadButton.tsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import { Button } from "primereact/button";
 import { useFileSystemStore } from "@entities/fileSystem";
+import { useFileBrowserStore } from "@features/fileBrowser";
 
 interface UploadButtonProps {
   className?: string;
@@ -8,16 +9,17 @@ interface UploadButtonProps {
 
 export const UploadButton: React.FC<UploadButtonProps> = ({ className }) => {
   const uploadFile = useFileSystemStore((state) => state.uploadFile);
+  const currentFolderId = useFileBrowserStore((s) => s.currentFolderId);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleUploadClick = () => {
     fileInputRef.current?.click();
   };
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
-      uploadFile(file);
+      await uploadFile(file, currentFolderId);
       e.target.value = "";
     }
   };
